adee92bb63ed62cdbc3a3b56b7c59400460c3a06
Kori Ditmeyer <kori.ditmeyer@gmail.com>
2023-10-02 12:31:38 +0900

    Update particles error

--- javascript/particles.js
+++ javascript/particles.js
@@ -1,6 +1,6 @@
 particlesJS.load(
   "particles-js",
-  "../assets/json/particlesjs-config.json",
+  "./assets/json/particlesjs-config.json",
   function () {
     console.log("callback - particles.js config loaded");
   }
