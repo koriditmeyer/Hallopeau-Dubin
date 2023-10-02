5283aae3af828cd3e6f0ec96207aae544e0a6e8d
Kori Ditmeyer <kori.ditmeyer@gmail.com>
2023-10-02 12:37:27 +0900

    Update particles error

--- javascript/particles.js
+++ javascript/particles.js
@@ -1,6 +1,6 @@
 particlesJS.load(
   "particles-js",
-  "./assets/json/particlesjs-config.json",
+  "/assets/json/particlesjs-config.json", // Use an absolute path for live deployement
   function () {
     console.log("callback - particles.js config loaded");
   }
